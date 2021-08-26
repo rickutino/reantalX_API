76540586a2cc9907a5c069c727c13b36fee9f64c
rickutino <rickutino@gmail.com>
2021-08-26 23:32:31 +0900

    fix sintax

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
-import express from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
+import express from "express";
+
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 
@@ -7,5 +8,5 @@ app.use(express.json());
 app.use(categoriesRoutes);
 
 app.listen(3333, () => {
-	console.log('Server started on port 3333');
+  console.log("Server started on port 3333");
 });
