3aa640c3c3e79d69973b929a43bfc1aeedfd40ab
rickutino <rickutino@gmail.com>
2021-08-29 23:20:06 +0900

    add Category service

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -14,7 +15,9 @@ categoriesRoutes.get("/", (request, response) => {
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+
+  createCategoryService.execute({ name, description });
 
   return response.status(201).send();
 });
