58f51eaa54ee68bdc3f0ff9254e7b40144892f95
rickutino <rickutino@gmail.com>
2021-09-19 12:17:23 +0900

    refactoring to typeorm config

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import multer from "multer";
 
-import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import createCategoryController from "../modules/cars/useCases/createCategory";
 import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/ListCategory";
 
@@ -11,12 +11,12 @@ const upload = multer({
   dest: "./tmp",
 });
 
-categoriesRoutes.get("/", (request, response) => {
-  return listCategoriesController.handle(request, response);
+categoriesRoutes.post("/", (request, response) => {
+  return createCategoryController().handle(request, response);
 });
 
-categoriesRoutes.post("/", (request, response) => {
-  return createCategoryController.handle(request, response);
+categoriesRoutes.get("/", (request, response) => {
+  return listCategoriesController.handle(request, response);
 });
 
 categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
