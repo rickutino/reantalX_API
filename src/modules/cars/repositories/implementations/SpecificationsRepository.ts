ddc04c858e828720366c0739f4afe20662122e74
rickutino <rickutino@gmail.com>
2021-09-20 21:35:12 +0900

    updated swagger documentation

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -7,29 +7,29 @@ import {
 } from "../ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
-  private specification: Repository<Specification>;
+  private repository: Repository<Specification>;
 
   constructor() {
-    this.specification = getRepository(Specification);
+    this.repository = getRepository(Specification);
   }
 
   async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
-    const specification = this.specification.create({
+    const specification = this.repository.create({
       name,
       description,
     });
 
-    await this.specification.save(specification);
+    await this.repository.save(specification);
   }
 
   async list(): Promise<Specification[]> {
-    const specifications = await this.specification.find();
+    const specifications = await this.repository.find();
 
     return specifications;
   }
 
   async findByName(name: string): Promise<Specification> {
-    const specification = await this.specification.findOne({ name });
+    const specification = await this.repository.findOne({ name });
 
     return specification;
   }
