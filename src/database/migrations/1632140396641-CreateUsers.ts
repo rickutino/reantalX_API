ab379b0b2f35ab7fb12492e63df6389471a65e72
rickutino <rickutino@gmail.com>
2021-09-23 12:57:37 +0900

    remove username Column in Users Table

--- src/database/migrations/1632140396641-CreateUsers.ts
+++ src/database/migrations/1632140396641-CreateUsers.ts
@@ -15,11 +15,6 @@ export class CreateUsers1632140396641 implements MigrationInterface {
             type: "varchar",
           },
           {
-            name: "username",
-            type: "varchar",
-            isUnique: true,
-          },
-          {
             name: "password",
             type: "varchar",
           },
