9b3b60c27c255f38abee25a0619e23f662b976d8
rickutino <rickutino@gmail.com>
2021-11-08 22:12:30 +0900

    add car repository interface

---
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -0,0 +1,8 @@
+import { ICreateCarDTO } from "@modules/cars/dtos/ICreateCarTDO";
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+
+interface ICarsRepository {
+  create(data: ICreateCarDTO): Promise<Car>;
+  findByLicensePlate(license_plate: string): Promise<Car>;
+}
+export { ICarsRepository };
