a2e0502e3ea246b9eeedeffa37369cc4522582ab
rickutino <rickutino@gmail.com>
2021-11-17 20:49:13 +0900

    add interface Cars repositories

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -4,5 +4,6 @@ import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 interface ICarsRepository {
   create(data: ICreateCarDTO): Promise<Car>;
   findByLicensePlate(license_plate: string): Promise<Car>;
+  list(category_id?: string, brand?: string, name?: string): Promise<Car[]>;
 }
 export { ICarsRepository };
