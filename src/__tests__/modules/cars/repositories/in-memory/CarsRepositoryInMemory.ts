19211b92d86bcc944e1dc3d64cebae777f638d53
rickutino <rickutino@gmail.com>
2021-11-21 09:51:28 +0900

    add findById Method in CarsRepositoryInMemory

--- src/__tests__/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/__tests__/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -1,4 +1,4 @@
-import { ICreateCarDTO } from "@modules/cars/dtos/ICreateCarTDO";
+import { ICreateCarDTO } from "@modules/cars/dtos/ICreateCarDTO";
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 
@@ -33,6 +33,10 @@ class CarsRepositoryInMemory implements ICarsRepository {
 
   async findByLicensePlate(license_plate: string): Promise<Car> {
     return this.cars.find((car) => car.license_plate === license_plate);
+  }
+
+  async findById(id: string): Promise<Car> {
+    return this.cars.find((car) => car.id === id);
   }
 
   async list(
