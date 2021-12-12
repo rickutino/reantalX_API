1eee687a14443debbe0adb0f2936d7a41ff15541
rickutino <rickutino@gmail.com>
2021-12-12 11:42:41 +0900

    Add Rental feauture route and provider

---
+++ src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -0,0 +1,27 @@
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
+import { IDateProvider } from "../IDateProvider";
+
+dayjs.extend(utc);
+
+class DayjsDateProvider implements IDateProvider {
+  // compara o dia de devolucao esperada com a data atual
+  compareInHours(start_date: Date, end_date: Date): number {
+    const start_date_utc = this.convertToUTC(start_date);
+    const end_date_utc = this.convertToUTC(end_date);
+
+    return dayjs(end_date_utc).diff(start_date_utc, "hours");
+  }
+
+  // utc formata num horario que ao adicionar 1 day conta como 24 horas.
+  convertToUTC(date: Date): string {
+    return dayjs(date).utc().local().format();
+  }
+
+  dateNow(): Date {
+    return dayjs().toDate();
+  }
+}
+
+export { DayjsDateProvider };
