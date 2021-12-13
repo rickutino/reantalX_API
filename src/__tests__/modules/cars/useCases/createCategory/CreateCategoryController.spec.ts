0b37fc7e8d8db40a1bb88849d0c1cebed8ec4ef5
rickutino <rickutino@gmail.com>
2021-12-13 22:53:35 +0900

    should be able to create a new category

---
+++ src/__tests__/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -0,0 +1,51 @@
+import { hash } from "bcryptjs";
+import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm";
+
+let connection: Connection;
+
+describe("Create Category Controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    // Por utilizar database de test rodar as migrations.
+    await connection.runMigrations();
+
+    const id = uuidV4();
+    const password = await hash("admin", 8);
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+      values('${id}', 'admin', 'admin@rentalx.co.jp', '${password}', true, 'now()', '125 ひ 1564')
+      `
+    );
+  });
+
+  // Apos o teste dar drop no database e cancelar a ligacao com o mesmo.
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("should be able to create a new category", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentalx.co.jp",
+      password: "admin",
+    });
+
+    const { token } = responseToken.body.token;
+
+    const response = await request(app)
+      .post("/categories")
+      .send({
+        name: "Category Supertest",
+        description: "Category Supertest",
+      })
+      .set({ Authorization: `Bearer ${token}` });
+
+    expect(response.status).toBe(201);
+  });
+});
