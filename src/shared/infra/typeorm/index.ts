4fe3b0262dc5c04e0ccf7b69fa5647f193bea889
rickutino <rickutino@gmail.com>
2021-12-13 22:53:11 +0900

    setting test database

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -5,7 +5,12 @@ export default async (host = "database"): Promise<Connection> => {
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host, // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+      // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+      host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      database:
+        process.env.NODE_ENV === "test"
+          ? "rentalx_test"
+          : defaultOptions.database,
     })
   );
 };
