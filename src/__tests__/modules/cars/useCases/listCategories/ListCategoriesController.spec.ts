ced44f80a3081cf0b7aa51c77b52f5cd8313e86d
rickutino <rickutino@gmail.com>
2021-12-15 21:56:47 +0900

    add scenario test ListCategoryController

---
+++ src/__tests__/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -0,0 +1,58 @@
+import { hash } from "bcryptjs";
+import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm";
+
+let connection: Connection;
+
+describe("List Categories Controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    // Por utilizar database de test rodar as migrations.
+    await connection.runMigrations();
+
+    const id = uuidV4();
+    const password = await hash("admin", 8);
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+      values('${id}', 'admin', 'admin@rentalx.co.jp', '${password}', true, 'now()', '125 ひ 1564')
+      `
+    );
+  });
+
+  // Após o teste dar drop no database e cancelar a ligacao com o mesmo.
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
+  it("should be able to list all categories", async () => {
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentalx.co.jp",
+      password: "admin",
+    });
+
+    const { token } = responseToken.body;
+
+    await request(app)
+      .post("/categories")
+      .send({
+        name: "Category Supertest",
+        description: "Category Supertest",
+      })
+      .set({ Authorization: `Bearer ${token}` });
+
+    const response = await request(app).get("/categories");
+
+    console.log(response.body);
+
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBe(1);
+    expect(response.body[0]).toHaveProperty("id");
+    expect(response.body[0].name).toEqual("Category Supertest");
+  });
+});
