0e65262b40fd393c372d495e23257622eb8f1e7b
GabrielUtino <gabrielutino96@gmail.com>
2021-12-31 22:03:25 +0900

    add  devolution rental feature

--- src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalUseCase.ts
@@ -4,6 +4,7 @@ import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 import { AppError } from "@shared/errors/AppError";
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 
 interface IRequest {
   id: string;
@@ -20,12 +21,47 @@ class DevolutionRentalUseCase {
     private dateProvider: IDateProvider
   ) {}
 
-  async execute({ id, user_id }: IRequest) {
+  async execute({ id, user_id }: IRequest): Promise<Rental> {
     const rental = await this.rentalRepository.findById(id);
+    const car = await this.carsRepository.findById(id);
+
+    let total = 0;
+    const minimum_daily = 1;
 
     if (!rental) {
       throw new AppError("Rental does not exists!");
     }
+
+    let daily = this.dateProvider.compareInDays(
+      rental.start_date,
+      this.dateProvider.dateNow()
+    );
+
+    if(daily <= 0) {
+      daily = minimum_daily;
+    }
+
+    const dateNow = this.dateProvider.dateNow();
+
+    const delay = this.dateProvider.compareInDays(
+      dateNow,
+      rental.expected_return_date
+    );
+
+    if(delay > 0) {
+      const calculate_fine = delay * car.fine_amount;
+      total = calculate_fine;
+    }
+
+    total += daily * car.daily_rate;
+
+    rental.end_date = this.dateProvider.dateNow();
+    rental.total = total;
+
+    await this.rentalRepository.create(rental);
+    await this.carsRepository.updateAvailable(car.id, true);
+
+    return rental;
   }
 }
 
