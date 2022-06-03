ffd8698b59329272cdaecf2890a267d50a56494d
rickutino <rickutino@gmail.com>
2022-06-03 12:55:06 +0900

    fix: rename database used in this app of jest

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -9,7 +9,7 @@ export default async (host = "database"): Promise<Connection> => {
       host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
-          ? "rentalx_test"
+          ? "rentalx"
           : defaultOptions.database,
     })
   );
