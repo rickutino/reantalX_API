4961ef268c8c9e22d7788a8378dc5b11f12fd3a8
rickutino <rickutino@gmail.com>
2022-07-30 12:34:01 +0900

    feat: add user token repository create

--- src/modules/accounts/infra/typeorm/entities/UserTokens.ts
+++ src/modules/accounts/infra/typeorm/entities/UserTokens.ts
@@ -1,4 +1,6 @@
 import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import { v4 as uuidv4 } from 'uuid';
+
 import { User } from "./User";
 
 @Entity("users_tokens")
@@ -21,5 +23,11 @@ class UserTokens {
 
   @CreateDateColumn()
   created_at: Date;
+  
+  constructor() {
+    if(!this.id) {
+      this.id = uuidv4();
+    }
+  }
 }
 export { UserTokens };
