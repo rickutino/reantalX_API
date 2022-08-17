1110ee0f6ccfac54dbcf74919952a16d4e41135e
Ricardo Utino <rickutino@gmail.com>
2022-08-17 04:01:15 +0000

    chore: add refresh token repository

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+// hash build by md5
+export default {
+  secret_token: "518eefa1df679e103bc4ac57a12df2c1",  
+  expires_in_token: "15m",
+  secret_refresh_token: "43ce7b04d9d92da544ac4ace40d9869d",
+  expires_in_refresh_token: "30d"
+}
--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -4,6 +4,9 @@ import { inject, injectable } from "tsyringe";
 
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
 import { AppError } from "@shared/errors/AppError";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import auth from "@config/auth";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
 
 interface IRequest {
   email: string;
@@ -22,11 +25,16 @@ interface IResponse {
 class AuthenticateUserUseCase {
   constructor(
     @inject("UsersRepository")
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    @inject("UsesrsTokensRepository")
+    private usersTokensReporitory: IUsersTokensRepository,
+    @inject("DayjsDateProvider")
+    private dayjsDateProvider: IDateProvider
   ) {}
 
   async execute({ email, password }: IRequest): Promise<IResponse> {
     const user = await this.usersRepository.findByEmail(email);
+    const { expires_in_token, secret_refresh_token, secret_token, expires_in_refresh_token} = auth
 
     if (!user) {
       throw new AppError("Email or password incorrect!");
@@ -39,10 +47,22 @@ class AuthenticateUserUseCase {
     }
 
     // Segundo parametro gerador md5.
-    const token = sign({}, "518eefa1df679e103bc4ac57a12df2c1", {
+    const token = sign({}, secret_token, {
       subject: user.id,
-      expiresIn: "1d",
+      expiresIn: expires_in_token,
     });
+
+    const refresh_token = sign({ email }, secret_refresh_token, {
+      subject: user.id,
+      expiresIn: expires_in_refresh_token
+    })
+
+    await this.usersTokensReporitory.create({
+      user_id: user.id,
+      expires_date,
+      refresh_token,
+
+    })
 
     const tokenReturn: IResponse = {
       token,
