d78e4cd26c7d0facfd7460a032aa4e4c2722db45
rickutino <rickutino@gmail.com>
2022-08-22 18:16:05 +0900

    feat: create refresh token UseCase

--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -7,6 +7,10 @@ interface IUsersTokensRepository {
     refresh_token,
     expires_date,
   }: ICreateUserTokenDTO): Promise<UserTokens>;
+
+  findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserTokens>;
+
+  deleteById(id: string): Promise<void>;
 }
 
 export { IUsersTokensRepository };
