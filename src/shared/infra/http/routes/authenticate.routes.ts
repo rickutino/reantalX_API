db76e6168322d1f8dc47735feaa6af1880721790
rickutino <rickutino@gmail.com>
2022-08-24 12:51:55 +0900

    feat: add refresh token Controller

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { AuthenticateUserController } from "@modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+import { RefreshTonkenController } from "@modules/accounts/useCases/refreshToken/RefreshTokenController";
 
 const authenticateRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
+const refreshTonkenController = new RefreshTonkenController();
 
 authenticateRoutes.post("/sessions", authenticateUserController.handle);
+authenticateRoutes.post("/refresh_token", refreshTonkenController.handle);
 
 export { authenticateRoutes };
