6d9fc3b881d64380c0a2f78ccdcf0d03ed04a233
rickutino <rickutino@gmail.com>
2022-08-26 12:34:23 +0900

    feat: add SendForgotPasswordMail UseCase

---
+++ src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController.ts
@@ -0,0 +1,17 @@
+import { request, Request } from "express";
+import { container } from "tsyringe";
+import { SendForgotPasswordMailUseCase } from "./SendForgotPasswordMailUseCase";
+
+class SendForgotPasswordMailController {
+   async handle(resquest: Request, response: Response): Promise<Response> {
+    const { email } = request.body;
+
+    const sendForgotPasswordMailUseCase = container.resolve(SendForgotPasswordMailUseCase);
+
+    await sendForgotPasswordMailUseCase.execute(email);
+
+    return response.json();
+  }
+}
+
+export { SendForgotPasswordMailController };
